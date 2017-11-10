8c0dc8263a4e164674ac206913a6ca24ab5e2c43
Adrian Kawecki <adrian.g.kawecki@gmail.com>
2017-11-11 02:16:34 +1100

    Update

---
+++ Dungeons of Aarkos/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject button1;
+	public GameObject button2;
+	public GameObject button3;
+	public GameObject button4;
+	public GameObject button5;
+	void Awake()
+	{
+		button4.SetActive(false);
+		button5.SetActive(false);
+	}
+	// Use this for initialization
+	void Start () {
+		//button4.SetActive(false);
+		//button5.SetActive(false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void characterClass()
+	{
+		button1.SetActive(false);
+		button2.SetActive(false);
+		button3.SetActive(false);
+		button4.SetActive(true);
+		button5.SetActive(true);
+	}
+
+
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+
+}
---
+++ Dungeons of Aarkos/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed;
+	public Sprite sprite1;
+	public Sprite sprite2;
+	public Sprite sprite3;
+	public Sprite sprite4;
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			transform.Translate (new Vector3 (-moveSpeed, 0,0));
+			spriteRenderer.sprite = sprite1;
+		}
+
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			transform.Translate(new Vector3(moveSpeed,0,0));
+			spriteRenderer.sprite = sprite2;
+		}
+
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			transform.Translate(new Vector3(0,moveSpeed,0));
+			spriteRenderer.sprite = sprite3;
+		}
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			transform.Translate(new Vector3(0,-moveSpeed,0));
+			spriteRenderer.sprite = sprite4;
+		}
+	}
+}
