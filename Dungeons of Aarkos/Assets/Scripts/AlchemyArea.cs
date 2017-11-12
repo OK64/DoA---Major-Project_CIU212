aa4fb8886ecbb6e498c9b73c50d4fec735a066c5
Adrian Kawecki <adrian.g.kawecki@gmail.com>
2017-11-13 00:59:13 +1100

    v0.2

---
+++ Dungeons of Aarkos/Assets/Scripts/AlchemyArea.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlchemyArea : MonoBehaviour
+{
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update()
+	{
+		_DistanceDetection(2);
+	}
+
+	public void _DistanceDetection(int dist)
+	{
+		GameObject player = GameObject.FindObjectOfType<PlayerMovement>().gameObject;
+		
+		float dX = player.transform.position.x - this.transform.position.x;
+		float dY = player.transform.position.y - this.transform.position.y;
+		if(dX < 0)
+			dX *= -1;
+		if(dY < 0)
+			dY *= -1;
+		
+		float diff = ((int)(Mathf.Sqrt(Mathf.Pow(dX, 2) + Mathf.Pow(dY, 2))*10));
+
+		if(diff <= dist)
+		{
+			Debug.Log("In_Range: " + diff);
+		}
+	}
+}
